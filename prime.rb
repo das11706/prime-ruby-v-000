67f92a4b771c8bc66b072946b7b603c285cc2aa8
Dan Suarez <suarez.daniel101@gmail.com>
2019-02-26 00:00:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 # Add  code here!
 
-#def prime?(num)
-  #start = 2
-  #(start...num).none?{|int| num % int == 0}
-    #if num < 2
-      #false
-    #elsif (int % 1 == 0) && (int % int == 0)
-      #true
-  #end
+def prime?(num)
+  if num >= 2
+  (2...num).none?{|int| num % int == 0}
+  end
+end
+
+#def prime?(number)
+#number >= 2 && (2...number).none? { |n| number % n == 0 }
 #end
 
 #def to_array
@@ -18,9 +18,3 @@
 #none?
 #Imagine the opposite of #all?, a method #none?, where we are interested in none of the elements in a collection producing a true expression within the block passed to #none?.
 #[1,3].none?{|i| i.even?} #=> true
-
-
-
-def prime?(number)
-number >= 2 && (2...number).none? { |n| number % n == 0 }
-end
